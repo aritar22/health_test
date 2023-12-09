a881751f44010d0229fe480ef4c11cd2030ba3c3
aritar22 <aritaar22@gmail.com>
2023-12-09 15:47:01 -0500

    Screen1

--- healthtest - Copy.py
+++ healthtest - Copy.py
@@ -1,5 +1,6 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QRadioButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit, QGroupBox
+from PyQt5.QtCore import Qt, QTimer, QRegExp
+from PyQt5.QtGui import QIntValidator
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit, QGroupBox
 
 app = QApplication([])
 win = QWidget()
@@ -24,14 +25,49 @@ scr2_widgets = {
     "age_label": QLabel("Enter your age:"),
     "age_input": QLineEdit(),
     "instructions1": QLabel("Instructions"),
-    "test_1": QPushButton("Start the first test"),
+    "test_1": QPushButton("Start the test"),
     "p1_input": QLineEdit(),
     "instructions2": QLabel("Instructions"),
-    "results_btn": QPushButton("Show Results")  # Add the results button
+    "results_btn": QPushButton("Show Results")
 }
+
+int_validator = QIntValidator()
+
+scr2_widgets["age_input"].setValidator(int_validator)
+scr2_widgets["p1_input"].setValidator(int_validator)
 for widget in scr2_widgets.values():
     screen2.addWidget(widget)
-scr2_widgets["results_btn"].clicked.connect(lambda: on_scr2_btn())  # Connect the button to function
+
+scr2_widgets["results_btn"].clicked.connect(lambda: on_scr2_btn())
+timer = QTimer()
+time_label = QLabel()
+screen2.addWidget(time_label)
+
+time_elapsed = 0
+
+def start_timer():
+    timer.start(1000)
+    global time_elapsed
+    time_elapsed = 0
+    timer.timeout.connect(update_timer)
+
+def update_timer():
+    global time_elapsed
+    time_elapsed += 1
+    time_label.setText(f"Time Elapsed: {time_elapsed} seconds")
+
+scr2_widgets["test_1"].clicked.connect(start_timer)
+
+def on_scr1_btn():
+    scr1.hide()
+    scr2.show()
+    scr3.hide()
+
+def on_scr2_btn():
+    # Add logic to handle screen 2 results button functionality
+    pass
+
+screen1btn.clicked.connect(on_scr1_btn)
 
 # create screen 3 interface
 screen3 = QVBoxLayout()
